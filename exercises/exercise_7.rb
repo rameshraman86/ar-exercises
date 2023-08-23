5c073bc300eb70e4741425bb12507287f02bc690
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-08-23 15:55:58 -0400

    exer 7 complete

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,41 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+# # Your code goes here ...
+# Add validations to two models to enforce the following business rules:
+# Employees must always have a first name present
+# Employees must always have a last name present
+# Employees have a hourly_rate that is a number (integer) between 40 and 200
+# Employees must always have a store that they belong to (can't have an employee that is not assigned a store)
+
+class Employee < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, inclusion: { in: 40..200, message: "must be between 40 and 200" }
+  validates_associated :store
+end
+
+# Stores must always have a name that is a minimum of 3 characters
+# Stores have an annual_revenue that is a number (integer) that must be 0 or more
+# BONUS: Stores must carry at least one of the men's or women's apparel (hint: use a custom validation method - don't use a Validator class)
+# Ask the user for a store name (store it in a variable)
+# Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
+# Display the error messages provided back from ActiveRecord to the user (one on each line) after you attempt to save/create the record
+
+class Store < ActiveRecord::Base
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :must_carry_apparel
+
+  def must_carry_apparel
+    if !mens_apparel && !womens_apparel
+      errors.add(:mens_apparel, "must carry mens or womens apparel")
+      errors.add(:womens_apparel, "must carry mens or womens apparel")
+    end
+  end
+end
+
+puts "Enter a store name:"
+store_name = gets.chomp
+new_store = Store.create(name: store_name)
+puts new_store.errors.full_messages

3b34b2d035de2dc64a090a908db0e5434ca85a5d
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-08-23 14:50:09 -0400

    exer 3 complete

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,20 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+class Exercise3
+
+  attr_accessor :store3
+
+  def initialize(store3)
+    @store3 = store3
+  end
+
+  def delete()
+    @store3.destroy
+  end
+
+end
+
+exer3 = Exercise3.new(Store.find_by(id: 3))
+exer3.delete()
+puts Store.count
